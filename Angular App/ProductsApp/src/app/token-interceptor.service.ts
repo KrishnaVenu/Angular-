e795bc4595b40c46c70566da79958e4076e73903
KrishnaVenu <62461769+KrishnaVenu@users.noreply.github.com>
2020-08-02 19:35:51 +0530

    Add files via upload

---
+++ Angular App/ProductsApp/src/app/token-interceptor.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, Injector, inject } from '@angular/core';
+import {HttpInterceptor} from '@angular/common/http';
+import {UsersService} from './users.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenInterceptorService implements HttpInterceptor {
+   constructor(private injector: Injector) { }
+  intercept(req,next){
+    let usersService = this.injector.get(UsersService)
+    let tokenizedReq = req.clone(
+      {
+        setHeader:{
+          Authorization: `Bearer ${usersService.getToken()}` 
+        }
+      })
+    return next.handle(tokenizedReq)
+  }
+ }
