7c5becade4db12fe39a841be63121c6b12f2fb54
KrishnaVenu <62461769+KrishnaVenu@users.noreply.github.com>
2020-08-03 00:22:03 +0530

    Update app.module.ts

--- Angular App/ProductsApp/src/app/app.module.ts
+++ Angular App/ProductsApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientModule,HTTP_INTERCEPTORS} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +13,9 @@ import { LoginComponent } from './login/login.component';
 import { FooterComponent } from './footer/footer.component';
 import { UpdateComponent } from './update/update.component';
 import { ProductService} from './product.service';
+import {UsersService} from './users.service';
+import {ProductGuard} from './product.guard';
+import {TokenInterceptorService} from './token-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -31,7 +34,13 @@ import { ProductService} from './product.service';
     HttpClientModule,
     FormsModule
   ],
-  providers: [ ProductService],
+   providers: [ ProductService,UsersService, ProductGuard,
+   {
+    provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptorService,
+    multi: true
+   }
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
