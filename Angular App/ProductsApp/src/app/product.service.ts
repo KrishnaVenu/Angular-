94f7ab297ef48d681341d8848eab44826a519b4d
KrishnaVenu <62461769+KrishnaVenu@users.noreply.github.com>
2020-08-03 00:19:53 +0530

    Update product.service.ts

--- Angular App/ProductsApp/src/app/product.service.ts
+++ Angular App/ProductsApp/src/app/product.service.ts
@@ -1,23 +1,32 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import{ProductModel} from './product-list/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-
+private product:ProductModel
   constructor(private http:HttpClient) { }
   getProducts(){
-    return this.http.get('http://localhost:3000/products');
+    return this.http.get('http://localhost:3000/p/products');
   }
   newProduct(item){
-    return this.http.post('http://localhost:3000/insert',{'product':item})
+    console.log('call');
+    return this.http.post('http://localhost:3000/p/insert',{'product':item})
     .subscribe(data=>{console.log(data)})
   }
+  // set(item){
+  // this.product = item;
+  // }
+  //  check(){
+  //    return this.product;
+  // }
   updateProduct(item){
     console.log(item);
-    return this.http.post('http://localhost:3000/update',{'product':item})
+    return this.http.post('http://localhost:3000/update/edit',{'product':item})
     .subscribe(data=>{console.log(data)})
   }
+  
  }
  
