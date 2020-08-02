93ef574cfbd7142b1b878d43f728c75e7e8552ba
KrishnaVenu <62461769+KrishnaVenu@users.noreply.github.com>
2020-08-03 00:16:44 +0530

    Update users.service.ts

--- Angular App/ProductsApp/src/app/users.service.ts
+++ Angular App/ProductsApp/src/app/users.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  private _registerUrl = 'http://localhost:3000/admin/register';
+  private _loginUrl = 'http://localhost:3000/admin/login';
 
-  constructor() { }
+  constructor(private http: HttpClient,private router: Router) { }
+  registerUser(user){
+    return this.http.post(this._registerUrl,user)
+  }
+  loginUser(user){
+    return this.http.post(this._loginUrl,user)
+  }
+  loggedIn(){
+    return !!localStorage.getItem('token')
+  }
+  getToken(){
+     return localStorage.getItem('token')
+  }
 }
