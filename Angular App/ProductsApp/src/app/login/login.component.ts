11b91c7f139ccf93752fe64ccf972ddd77b863b3
KrishnaVenu <62461769+KrishnaVenu@users.noreply.github.com>
2020-08-03 00:06:48 +0530

    Update login.component.ts

--- Angular App/ProductsApp/src/app/login/login.component.ts
+++ Angular App/ProductsApp/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {UsersService} from '../users.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+ loginUserDetails = {email:'',password: ''};
 
-  constructor() { }
+  constructor(private usersService: UsersService,private router: Router) { }
 title:String = 'Login';
+loginUser(){
+  //console.log(this.loginUserDetails)
+    this.usersService.loginUser(this.loginUserDetails)
+    .subscribe(
+       
+ res => { 
+    // console.log('token')
+    localStorage.setItem('token',res['token'])
+    this.router.navigate(['/product'])
+   },
+   err => console.log(err)
+   )
+ }
   ngOnInit(): void {
   }
 
