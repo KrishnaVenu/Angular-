835ffc8d74c46666df920c32086e9cf94a97b3d5
KrishnaVenu <62461769+KrishnaVenu@users.noreply.github.com>
2020-08-02 13:58:58 +0530

    Update Registrationdata.js

--- Angular App/PRODUCTS/src/model/Registrationdata.js
+++ Angular App/PRODUCTS/src/model/Registrationdata.js
@@ -2,13 +2,9 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/Product');
 const Schema = mongoose.Schema;
 const RegistrationSchema = new Schema({
-         Name: String,
-        Email: String,
-     Password: String,
-      Address: String,
-     Address2: String,
-      Country: String,
-        State: String
+     email: String,
+     password: String
+      
 });
-var Registrationdata = mongoose.model('product',RegistrationSchema);
+var Registrationdata = mongoose.model('user',RegistrationSchema);
 module.exports = Registrationdata;
